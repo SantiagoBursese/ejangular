140a8c8138e478de0fdfe1616ae9552421a9ffbd
SantiagoBursese <santi.churus@gmail.com>
2019-08-01 15:23:59 -0300

    Ultimos cambios en el proyecto

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ImagenComponent } from './imagen/imagen.component';
+import { TextoComponent } from './texto/texto.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ImagenComponent,
+    TextoComponent
   ],
   imports: [
     BrowserModule,
